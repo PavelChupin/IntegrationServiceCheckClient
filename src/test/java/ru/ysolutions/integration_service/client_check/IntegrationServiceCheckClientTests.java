4bf1303e70817b8a5b02f92f61074e9f6883fedd
Chupin Pevel <pavel.chupin@gmail.com>
2021-04-08 16:03:43 +0700

    Создание и настройка проекта

---
+++ src/test/java/ru/ysolutions/integration_service/client_check/IntegrationServiceCheckClientTests.java
@@ -0,0 +1,13 @@
+package ru.ysolutions.integration_service.client_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationServiceCheckClientTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
